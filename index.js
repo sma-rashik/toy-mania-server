c69c6ad4a3f9864d131f53f66dcd8ba784cf777a
Rashik <smarashik00@gmail.com>
2023-05-18 15:40:58 +0600

    mongodb added

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+console.log(process.env.DB_PASS);
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.aujohll.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aujohll.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
