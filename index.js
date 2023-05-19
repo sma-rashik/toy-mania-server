2daca8a1fabd650063edf6a1d03c0817bfd02032
Rashik <smarashik00@gmail.com>
2023-05-19 17:31:36 +0600

    crd done

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aujohll.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,18 +30,42 @@ async function run() {
         app.get('/toys', async (req, res) => {
             const cursor = toyCollection.find();
             const result = await cursor.toArray()
+            console.log(result);
             res.send(result)
         })
+        // Create   
+        app.post('/addToys', async (req, res) => {
+            const newToy = req.body;
+            console.log(newToy);
+            const result = await toyCollection.insertOne(newToy);
+            res.send(result);
+        });
+        // Read
+        app.get('/addToys', async (req, res) => {
+            const cursor = toyCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        });
+
 
         app.get('/toys/:id', async (req, res) => {
+
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
+            const query = { _id: new ObjectId(id) };
+            const toy = await toyCollection.findOne(query);
+            res.send(toy)
+        });
 
+        // delete
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const toy = await toyCollection.deleteOne(query);
+            res.send(toy)
+        });
 
+        // ...
 
-            const result = await toyCollection.findOne(query)
-            res.send(result)
-        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
